a5698611ab37d89ecd1622004d455a8e4488dffb
Leonardo <73667534+Leozin777@users.noreply.github.com>
2024-06-11 22:08:19 -0300

    filtro ok

--- ap2/lib/main.dart
+++ ap2/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ap2/data/services/character_service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'data/models/character.dart';
@@ -16,13 +17,28 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   CharacterService service = CharacterService();
-  late Future<List<Character>> c_characters;
-  late Future<List<Character>> c_charactersFiltered;
+  late Future<List<Character>> _charactersFuture;
+  late List<Character> _characters;
+  late List<Character> _charactersFiltered;
 
   @override
   void initState() {
     super.initState();
-    c_characters = service.getCharacters();
+    _charactersFuture = _getCharacters();
+  }
+
+  _filterCharacters(String filtro) {
+    setState(() {
+      _charactersFiltered = _characters
+          .where((element) => element.name.toLowerCase().contains(filtro.toLowerCase()))
+          .toList();
+    });
+  }
+
+  Future<List<Character>> _getCharacters() async {
+    _characters = await service.getCharacters();
+    _charactersFiltered = _characters;
+    return _characters;
   }
 
   @override
@@ -32,24 +48,45 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text("Home"),
         ),
-        body: FutureBuilder(
-            future: c_characters,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.separated(
-                    itemBuilder: (context, index) {
-                      return ListTile(title: Text(snapshot.data![index].name), leading: Image.network(snapshot.data![index].image),);
-                    },
-                    separatorBuilder: (context, int) {
-                      return const Divider();
-                    },
-                    itemCount: snapshot.data!.length);
-              }
-
-              if(snapshot.hasError) return const Text("Erro ao buscar dados");
-
-              return const CircularProgressIndicator();
-            }),
+        body: Column(
+          
+          children: [
+            TextField(
+              onChanged: (value) {
+                _filterCharacters(value);
+              },
+              decoration: const InputDecoration(
+                labelText: "Filtro",
+              ),
+            ),
+            Expanded(
+              child: FutureBuilder<List<Character>>(
+                  future: _charactersFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.separated(
+                          scrollDirection: Axis.vertical,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text(_charactersFiltered[index].name),
+                              leading:
+                                  Image.network(_charactersFiltered[index].image),
+                            );
+                          },
+                          separatorBuilder: (context, int) {
+                            return const Divider();
+                          },
+                          itemCount: _charactersFiltered.length);
+                    }
+              
+                    if (snapshot.hasError)
+                      return const Text("Erro ao buscar dados");
+              
+                    return const CircularProgressIndicator();
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
